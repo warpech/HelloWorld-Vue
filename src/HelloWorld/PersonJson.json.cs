5867dd07c57aa748359b947e1f1702e1033c8885
warpech <warpech@gmail.com>
2016-06-06 15:15:46 +0200

    step 4: add calculated property FullName

--- src/HelloWorld/PersonJson.json.cs
+++ src/HelloWorld/PersonJson.json.cs
@@ -4,6 +4,8 @@ namespace HelloWorld
 {
     partial class PersonJson : Json
     {
+        public string FullName => $"{FirstName} {LastName}";
+
         void Handle(Input.SaveTrigger action)
         {
             Transaction.Commit();
